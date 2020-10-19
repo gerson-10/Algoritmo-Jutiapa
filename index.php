b8045140cd4f2dceb5f18f942c4571dd66bd1d8c
gerson-10 <52856517+gerson-10@users.noreply.github.com>
2020-10-19 16:33:42 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Algoritmo Jutiapa</title>
+    <?php
+    include_once("./Layouts/style.php");
+    //include_once("./Layouts/inicio.php");
+    session_start();
+
+if (!isset($_SESSION['USUARIO_LOGUEADO'])){
+
+    //echo'<script type="text/javascript">  alert("usted no está logueado"); window.location.href="index.php";   </script>';
+
+}
+ //$_SESSION['LOGIN'];
+    
+   ?>
+  
+</head>
+<body>
+<div class="navbar navbar-expand-md navbar-dark bg-dark">
+<img  href="#" src="Images/logo.png" class="nav-link dropdown-toggle img-fluid" height="100px"
+                                    width="100px" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false"></img><br>
+  <a class="activ" href="index.php"><i class="fa fa-fw fa-home"></i> AlgoritmoJutiapa.net</a>&nbsp
+  <a href="principal.php"><i class="fa fa-desktop" aria-hidden="true"></i> Inicio</a><br>&nbsp
+  <a href="registrar.php"><i class="fa fa-fw fa-user"></i> Registrarse</a><br>&nbsp
+  <a href="informacion.php"><i class="fa fa-id-card" aria-hidden="true"></i> Informacion</a><br>&nbsp
+  <a class="active" href="cierre_session.php"><i class="fa fa-power-off" aria-hidden="true"></i> Cerrar Session</a>
+</div>
+<div align="center" class="container">
+        <h1>Algoritmo Jutiapa</h1>
+    </div>
+<div align="left" class="container">
+<img src="Images/creatividad.jpg" class="nav-link dropdown-toggle img-fluid" height="200px"
+                                    width="300px" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false"></img>
+</div>
+<div align="right" class="container">
+<img src="Images/logo.png" class="nav-link dropdown-toggle img-fluid" height="300px"
+                                    width="300px" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false"></img><br>
+</div>
+
+<form action="ValidarUsuario.php" method="post" class="login">
+		<h3 class="text-center">Inicio de sesión</h3>
+		<div class="form-group">
+			<label for="Nombre">Usuario</label>
+			<input type="email" class="form-control" name="email" id="email" placeholder="Escribe tu correo" required="30">
+		</div>
+		<div class="form-group">
+			<label for="Password">Contraseña</label>
+			<input type="password" class="form-control" name="pwd" id="pwd" placeholder="Escribe tu Contraseña" required size="16">
+		</div>
+		<p class="text-center">
+			<button class="btn btn-primary btn-block">Login</button>
+        </p>
+</form>
+<footer>
+	<?php 
+		//include 'Layouts/footer.php';
+	?>
+</footer>
+        <script src="Js/bootstrap.min.js"></script>
+        <script src="Js/jquery-3.4.1.min.js"></script>
+        <script src="Js/Css/main.js"></script>
+</body>
+</html>
---
+++ informacion.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Algoritmo Jutiapa</title>
+    <link rel="stylesheet" href="Css/datatables.min.css"/>
+    <link rel="stylesheet" href="Css/bootstrap.min.css">
+    <link rel="stylesheet" href="Css/main.css">
+    <link rel="stylesheet" href="Css/estilos_info.css">
+    <!--style type="text/css">
+        body {
+          background-image: url(Images/fondo5.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+    }
+    </style-->
+    <?php 
+        include 'Layouts/style.php';
+         ?>
+</head>
+<body>
+  <header>
+    <?php 
+        include 'Layouts/header.php';
+         ?>
+</header><br>
+        
+    <form><br>
+      <section class="contenido">
+			<h2></h2>
+			<article class="serv1">
+				<h4>Quienes somos?</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, voluptate. Placeat consequatur sapiente pariatur, ducimus amet dolore laborum quae itaque sunt culpa at ipsam aperiam illo, modi doloremque, vitae facere.
+				</p>
+				<figure>
+					<img src="Images/piensa.jpg" alt="">
+				</figure>
+			</article>
+			<article class="serv1">
+				<h4>Cual es nuestro objetivo?</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, voluptate. Placeat consequatur sapiente pariatur, ducimus amet dolore laborum quae itaque sunt culpa at ipsam aperiam illo, modi doloremque, vitae facere.
+				</p>
+				<figure>
+					<img src="Images/boy.jpg" alt="">
+				</figure>
+			</article>
+			<article class="serv1">
+				<h4>Nuestra Vision es:</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, voluptate. Placeat consequatur sapiente pariatur, ducimus amet dolore laborum quae itaque sunt culpa at ipsam aperiam illo, modi doloremque, vitae facere.
+				</p>
+				<figure>
+					<img src="Images/vision.jpg" alt="">
+				</figure>
+      </article>
+		</section>
+    </form><br>
+    <?php
+    include 'Layouts/footer.php';
+    ?>
+<script src="Js/bootstrap.min.js"></script>
+<script src="Js/jquery-3.4.1.min.js"></script>
+<script src="Js/datatables.min.js"></script>
+<script src="Js/Css/main.js"></script>
+</body>
+</html>
